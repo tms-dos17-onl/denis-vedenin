1fb3f5968af5dee26e7e0a47f01710d17f20ba41
denis-vedenin <denved98@mail.ru>
2023-08-02 19:59:55 +0300

    Add file *.sh

---
+++ HW3/hw3_1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Пишем скрипт о нагрузке ЦП и Памяти и о нахождение IP адресса
+cpuUsage=$(top -bn1 | awk '/Cpu/ { print $2}')
+memUsage=$(free -h | awk '/Mem/{print $3}'| sed 's/Gi/Гб/g')
+ipUsage=$(ip -4 a | grep /24 | sed 's/inet/ /g')
+# Вывод о нагрузках ЦП и Памяти и IP адресс
+  echo "Нагрузка ЦП: $cpuUsage%"
+  echo "Нагрузка Памяти: $memUsage"
+  echo "IP Адрес:  $ipUsage"
