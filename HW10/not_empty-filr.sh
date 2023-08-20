b97baca4caa9f10589503a7be5b011ea1b5887f8
denis-vedenin <denved98@mail.ru>
2023-08-20 21:40:34 +0300

    Done task 4

---
+++ HW10/not_empty-filr.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+FILENAME=$(ls)
+# Check if the file is empty
+#if [ ! -s "${FILENAME}" ]; then
+#    echo "$FILENAME" "File is empty"
+#fi
+
+if [ -f "${FILENAME}" ];then
+    if [ -s "${FILENAME}" ];then
+        echo "File ${FILENAME} существует и не пуст"
+    else
+        echo "File ${FILENAME} существует но пуст"
+    fi
+else
+    echo "File ${FILENAME} не существует"
+fi
+
