004721e66d04fa44bdb31e3455fa3e39f9dc992b
denis-vedenin <denved98@mail.ru>
2023-08-27 15:56:12 +0300

    Fix HW10

--- HW10/check-parity-shell.sh
+++ HW10/check-parity-shell.sh
@@ -5,7 +5,7 @@ num=$1
 if [ -z "$num" ]; then
 	echo "Аргумент не является числом"
 else
-	if [ $(expr "$num" % 2) -eq 0 ]; then
+	if [ $(("$num" % 2)) -eq 0 ]; then
 		echo "чётное"
 	else
 		echo "нечётное"
