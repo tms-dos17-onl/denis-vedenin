cfcda0c79946044f91573e5f34129f59b6a81cf1
denis-vedenin <denved98@mail.ru>
2023-08-27 15:28:16 +0300

    Update hw10

--- HW10/check-parity-shell.sh
+++ HW10/check-parity-shell.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 
-#ПРоверка на четность
-
-#if [ $(expr "$1" % 2) -eq 0 ]; then
-#    echo "чётное"
-#elif [ $(expr "$1" % 2) -ne 0 ]; then
-#    echo "нечётное"
-#else
-#    echo "Аргумент не является числом"
-#    exit 1
-#fi
-
 num=$1
 
 if [ -z "$num" ]; then
