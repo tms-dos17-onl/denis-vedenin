cebab14c8f680aa785ccc36649c8f5c62d77766d
denis-vedenin <denved98@mail.ru>
2024-01-27 18:10:08 +0300

    update file main.tf

--- diplom/.infra/vpc.tf
+++ diplom/.infra/vpc.tf
@@ -5,9 +5,9 @@ resource "google_compute_network" "vpc" {
 }
 
 # Subnet
-# resource "google_compute_subnetwork" "subnet" {
-#   name          = "${var.project_id}-subnet"
-#   region        = var.region
-#   network       = google_compute_network.vpc.name
-#   ip_cidr_range = "10.10.0.0/24"
-# }
+resource "google_compute_subnetwork" "subnet" {
+  name          = "${var.project_id}-subnet"
+  region        = var.region
+  network       = google_compute_network.vpc.name
+  ip_cidr_range = "10.0.0.0/24"
+}
