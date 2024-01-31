e62817803d5f477c890f4b6cb53fb5cad1d8e425
denis-vedenin <denved98@mail.ru>
2024-01-31 21:02:36 +0300

    Deploy test 3

--- diplom/.infra/vpc.tf
+++ diplom/.infra/vpc.tf
@@ -17,13 +17,13 @@ provider "google" {
 
 # VPC
 resource "google_compute_network" "vpc" {
-  name                    = "${var.project_id}-vpc"
+  name                    = "${var.project_id}-vpc-test"
   auto_create_subnetworks = "false"
 }
 
 # Subnet
 resource "google_compute_subnetwork" "subnet" {
-  name          = "${var.project_id}-subnet"
+  name          = "${var.project_id}-subnet-test"
   region        = var.region
   network       = google_compute_network.vpc.name
   ip_cidr_range = "10.10.0.0/24"
